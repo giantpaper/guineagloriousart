24cd14d2bdec3f0990e08ffa53297275577ed3ca
giantpaper <giantpaper@outlook.com>
2023-02-25 14:49:11 -0800

    Heading.vue - added css

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -9,6 +9,58 @@
 	</header>
 </template>
 
+<style lang="scss">
+	.h1,.h2,.h3,.h4,.h5,.h6 {
+		font-family: Poppins, sans-serif;
+		font-weight: 500;
+		text-align: center;
+		margin-top: 1rem;
+		.subtitle {
+			display: block;
+			font-size: 0.75em;
+			text-transform: uppercase;
+		}
+	}
+	.h1 {
+		font-size: 2rem;
+		&:after {
+			background: currentcolor;
+			content: '';
+			display: block;
+			width: 40px;
+			height: 1px;
+			margin: 2rem auto;
+		}
+		@at-root body[data-slug="home"] {
+			main header:first-of-type {
+				.h1 {
+					font-weight: bold;
+					font-size: 1.5rem;
+					margin-top: 0;
+					letter-spacing: 0.1rem;
+					text-transform: uppercase;
+					&:after {
+						display: none;
+					}
+				}
+			}
+		}
+		@at-root body[data-slug="gallery-image"] {
+			.h1 {
+				font-size: 3.4rem;
+				font-family: 'lindsey-signature', cursive;
+			}
+		}
+	}
+	.h2 {
+		font-size: 1.2rem;
+		letter-spacing: 0.125rem;
+	}
+	.h3 {
+		font-size: 1.5rem;
+	}
+</style>
+
 <script setup>
 defineProps({
 	blok: {
@@ -16,4 +68,5 @@ defineProps({
 		required: true,
 	},
 })
+window.htagClasses()
 </script>
