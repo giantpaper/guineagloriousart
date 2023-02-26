facb3e7c7bc83f6d8b8f24b95c12468a87039172
giantpaper <giantpaper@outlook.com>
2023-02-25 20:08:43 -0800

    Fixed issue with title tag

--- src/components/SEO.vue
+++ src/components/SEO.vue
@@ -1,6 +1,5 @@
 <script setup>
 	let globalTags = (tag, value, name) => {
-		value = value.replace(/%SITE_TITLE%/g, 'Guinea Glorious ART')
 		if (name !== undefined && name !== '') {
 			let existing_tag = document.querySelector(`${tag}[${name}]`)
 			if (existing_tag === null) {
@@ -25,12 +24,7 @@
 	})
 	
 	if (props.blok.TitleTag) {
-		let title = props.blok.TitleTag
-		console.log(title)
-		if (props.blok.ExcludeSiteTitle !== undefined && props.blok.ExcludeSiteTitle === true) {
-			title += ` | Guinea Glorious ART`
-		}
-		globalTags('title', `${props.blok.TitleTag}`)
+		globalTags('title', props.blok.TitleTag)
 	}
 	if (props.blok.MetaDescription) {
 		globalTags('meta', props.blok.MetaDescription, 'description')
